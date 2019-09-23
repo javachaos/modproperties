b0658b57bcc1446c29f81fba6acfdedab743bb13
Fred Laderoute <fredladeroute@gmail.com>
2019-09-22 20:48:14 -0400

    Fixed style and a couple bugs

--- src/main/java/com.github.javachaos/modproperties/utils/Constants.java
+++ src/main/java/com.github.javachaos/modproperties/utils/Constants.java
@@ -1,72 +1,78 @@
 package com.github.javachaos.modproperties.utils;
 
-import net.fabricmc.loader.api.FabricLoader;
-
 import java.io.File;
+import net.fabricmc.loader.api.FabricLoader;
 
 /**
  * Constants.
- * @author fred
  *
+ * @author fred
  */
 public final class Constants {
 
-    public static String MODID;
+  /**
+   * Encoding.
+   */
+  public static final String ENCODING = "UTF-8";
+  /**
+   * Application directory location.
+   */
+  public static final String GAME_DIR = FabricLoader.getInstance().getGameDirectory()
+      + File.separator;
+  public static final int TICKS_PER_SEC = 20;
+  private static String MODID;
+  /**
+   * Application Properties file name.
+   */
+  public static final String PROPERTY_FILE_NAME = Constants.GAME_DIR
+      + File.separator + getModID() + ".properties";
 
-    /**
-     * Private ctor.
-     * @param modid
-     */
-    private Constants(final String modid) {
-        this.MODID = modid;
+  /**
+   * Private ctor.
+   */
+  private Constants() {
+  }
+
+  /**
+   * Init the constants.
+   *
+   * @param modid the id for your mod
+   */
+  public static void init(final String modid) {
+    Constants.MODID = modid;
+  }
+
+  /**
+   * Returns the number of ticks per second.
+   *
+   * @param seconds the number of desired seconds to get ticks for
+   * @return the number of ticks per second
+   */
+  public static final int getTicksPerSec(int seconds) {
+    if (seconds >= 0) {
+      return seconds * TICKS_PER_SEC;
+    } else {
+      throw new RuntimeException("Error, ticks per second conversion out of bounds.");
     }
+  }
 
-    /**
-     * Init the constants.
-     * @param modid the id for your mod
-     */
-    public static void init(final String modid) {
-        new Constants(modid);
+  /**
+   * Returns the number of ticks per minute.
+   *
+   * @param minutes
+   *      the number of desired minutes to get ticks for
+   * @return the number of ticks per second
+   *
+   */
+  public static final int getTicksPerMin(int minutes) {
+    if (minutes >= 0) {
+      return minutes * getTicksPerSec(60);
+    } else {
+      throw new RuntimeException("Error, ticks per second conversion out of bounds.");
     }
+  }
 
-    /**
-     * Encoding.
-     */
-    public static final String ENCODING = "UTF-8";
-
-    /**
-     * Application directory location.
-     */
-    public static final String GAME_DIR = FabricLoader.getInstance().getGameDirectory()
-                    + File.separator;
-
-    /**
-     * Application Properties file name.
-     */
-    public static final String PROPERTY_FILE_NAME = Constants.GAME_DIR
-            + File.separator + MODID + ".properties";
-
-    public static final int TICKS_PER_SEC = 20;
-
-    /**
-     * Returns the number of ticks per second.
-     * @param seconds the number of desired seconds to get ticks for
-     * @return
-     *      the number of ticks per second
-     */
-    public static final int getTicksPerSec(int seconds) {
-        if (seconds >= 0 && seconds <= Integer.MAX_VALUE) {
-            return seconds * TICKS_PER_SEC;
-        } else {
-            throw new RuntimeException("Error, ticks per second conversion out of bounds.");
-        }
-    }
-
-    public static final int getTicksPerMin(int minutes) {
-        if (minutes >= 0 && minutes <= Integer.MAX_VALUE) {
-            return minutes * getTicksPerSec(60);
-        } else {
-            throw new RuntimeException("Error, ticks per second conversion out of bounds.");
-        }
-    }
+  public static final String getModID() {
+    return MODID;
+  }
 }
---
+++ src/main/java/com.github.javachaos/modproperties/utils/ModProperties.java
@@ -0,0 +1,9 @@
+package com.github.javachaos.modproperties.utils;
+
+import net.fabricmc.api.ModInitializer;
+
+public class ModProperties implements ModInitializer {
+  @Override
+  public void onInitialize() {
+  }
+}
